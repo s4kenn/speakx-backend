09f236a4de0d1bdaee658853715aa3b025a0cf19
Aditya Singh <yeezusgod01@gmail.com>
2025-01-22 11:11:39 +0530

    added received and send message length

--- server.js
+++ server.js
@@ -40,12 +40,9 @@ async function main() {
                     title: regexSearch
                 });
 
-                const totalPages = Math.ceil(total / limit);
-
                 callback(null, {
                     questions,
                     total,
-                    totalPages,
                     page,
                     limit
                 });
